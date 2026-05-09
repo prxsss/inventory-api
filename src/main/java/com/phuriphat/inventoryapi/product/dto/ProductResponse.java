06cc8ca2c785880f1a1c4b6b5a9a3c13fb1a6d0f
prxsss <9phuriphat.k@gmail.com>
2026-05-09 16:54:32 +0700

    refactor: remove unused imports in CreateProductRequest DTO

--- src/main/java/com/phuriphat/inventoryapi/product/dto/ProductResponse.java
+++ src/main/java/com/phuriphat/inventoryapi/product/dto/ProductResponse.java
@@ -1,9 +1,5 @@
 package com.phuriphat.inventoryapi.product.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Builder;
 import lombok.Data;
 
